031d5014253ba13b4aef9656c81246ca551eb1f3
MedvedevDev <a95.medvedev@gmail.com>
2023-11-09 08:17:48 +0100

    Serving static files

--- app.js
+++ app.js
@@ -5,16 +5,15 @@ const morgan = require('morgan')
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
-const app = express()
+const app = express();
 
 app.use(morgan('dev'));
-
 app.use(express.json()); // Data from the body is added to the request object
-
 app.use((req, res, next) => {
     console.log('MIDDLEWARE');
     next()
 })
+app.use(express.static(`${__dirname}/public`));
 
 // Mounting routes
 app.use('/api/v1/tours', tourRouter);
