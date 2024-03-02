801422f6fa63a3240e46d3174b6f0751972a293d
MedvedevDev <a95.medvedev@gmail.com>
2024-03-02 10:40:29 +0100

    Added almost all the fields to the Tour Model

--- models/tourModel.js
+++ models/tourModel.js
@@ -8,14 +8,50 @@ const tourSchema = new mongoose.Schema({
         required: [true, 'A tour must have a name'],
         unique: true
     },
-    rating: {
+    duration: {
+        type: Number,
+        required: [true, 'A tour must have a duration']
+    },
+    maxGroupSize: {
+        type: Number,
+        required: [true, "A tour must have a group size"]
+    },
+    difficulty: {
+        type: String,
+        required: [true, 'A tour must have a group difficulty']
+    },
+    ratingsAverage: {
         type: Number,
         default: 4.5
+    },
+    ratingsQuantity: {
+        type: Number,
+        default: 0
     },
     price: {
         type: Number,
         required: [true, 'A tour must have a price']
-    }
+    },
+    priceDiscount: Number,
+    summary: {
+        type: String,
+        trim: true,
+        required: [true, 'A tour must have a summary']
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    imageCover: {
+        type: String,
+        required: true
+    },
+    images: [String], // array of images
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    startDates: [Date] // array of dates
 });
 
 //Create model
