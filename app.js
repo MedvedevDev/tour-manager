f89710ba97de897df0ccd5427fb5d88935629a36
MedvedevDev <a95.medvedev@gmail.com>
2024-03-18 22:18:03 +0100

    Handling unhandled routes

--- app.js
+++ app.js
@@ -11,14 +11,21 @@ if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 app.use(express.json()); // Data from the body is added to the request object
-app.use((req, res, next) => {
-    console.log('MIDDLEWARE');
-    next()
-})
+// app.use((req, res, next) => {
+//     console.log('MIDDLEWARE');
+//     next()
+// })
 app.use(express.static(`${__dirname}/public`));
 
 // Mounting routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+    res.status(404).json({
+        status: 'fail',
+        message: `Cant find ${req.originalUrl} on this server!`
+    })
+})
 
 module.exports = app
