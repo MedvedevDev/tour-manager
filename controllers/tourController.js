23d5a217254c1d86d62d980099f11f37fad6d643
MedvedevDev <a95.medvedev@gmail.com>
2024-03-20 20:08:12 +0100

    404 not found errors

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const Tour = require('./../models/tourModel');
 const APIFeatures = require('./../utils/apiFeatures')
 const catchAsync = require('./../utils/catchAsync')
+const AppError = require('./../utils/appError')
 
 // Middleware
 exports.aliasTopTours = (req, res, next) => {
@@ -35,6 +36,10 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 exports.getTour = catchAsync(async (req, res, next) => {
         const tour = await Tour.findById(req.params.id);
         //Tour.findOne({_id: req.params.id}); -- analog
+
+        if(!tour) {
+            next(new AppError('No tour found with this ID', 404));
+        }
 
         res.status(200).json({
             status: 'success',
@@ -82,7 +87,11 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 })
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
-        await Tour.findByIdAndDelete(req.params.id, )
+        const tour = await Tour.findByIdAndDelete(req.params.id);
+        if (!tour) {
+            return next(new AppError('No tour found with that ID', 404));
+        }
+
         res.status(204).json({
             status: 'success',
             data: null
